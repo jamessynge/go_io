ae9471484f106f721508707769449865e0972562
James Synge <jamessynge@gmail.com>
2015-02-22 15:09:59 -0500

    Added ReadCloser struct, intending to replace gzipReadCloser later.

--- gzipio/gzip.go
+++ gzipio/gzip.go
@@ -24,3 +24,34 @@ func (grc *gzipReadCloser) Close() (err error) {
 	err = grc.gr.Close()
 	return
 }
+
+// ReadCloser embeds a gzip.Reader, changing the Close method so that it also
+// closes the underlying io.ReadCloser.
+type ReadCloser struct {
+	*gzip.Reader
+	src io.ReadCloser
+}
+
+func (p *ReadCloser) Close() error {
+	err := p.Reader.Close()
+	err2 := p.src.Close()
+	if err == nil {
+		err = err2
+	}
+	return err
+}
+
+// NewReadCloser creates a new ReadCloser (embeds a gzip.Reader). Returns an
+// error if gzip.NewReader fails (e.g. if src does not start with a valid
+// gzip header).
+func NewReadCloser(src io.ReadCloser) (*ReadCloser, error) {
+	r, err := gzip.NewReader(src)
+	if err != nil {
+		return nil, err
+	}
+	rc := &ReadCloser{
+		Reader: r,
+		src: src,
+	}
+	return rc, nil
+}
